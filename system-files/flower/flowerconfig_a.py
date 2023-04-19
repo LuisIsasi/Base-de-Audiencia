99e2474242b6e374c6aca494d68ae8292a9e1b1b
LuisIsasi <129641743+LuisIsasi@users.noreply.github.com>
2023-04-19 18:25:40 +0800

    mejoras-inciales

--- system-files/flower/flowerconfig_a.py
+++
@@ -1,6 +0,0 @@
-port = 3200
-logging = 'DEBUG'
-basic_auth = ['such[an]odd{bee}:just<watching>some,vegetables']
-broker_api = 'http://deploy:9w6&9*3s8U4K@aud-jobs01.geprod.amc:15672/api/'
-certfile = '/etc/nginx/ssl/star.atlanticmedia.com/star_atlanticmedia_com.crt'
-keyfile = '/etc/nginx/ssl/star.atlanticmedia.com/star_atlanticmedia_com.key'
--- system-files/flower/flowerconfig_dev.py
+++
@@ -1,6 +0,0 @@
-port = 3200
-logging = 'DEBUG'
-basic_auth = ['such[an]odd{bee}:just<watching>some,vegetables-on-dev']
-broker_api = 'http://deploy:9w6&9*3s8U4K@aud-jobs01.gedev.amc:15672/api/'
-certfile = '/etc/nginx/ssl/star.audb.govexec.com/star_audb_govexec_com.crt'
-keyfile = '/etc/nginx/ssl/star.audb.govexec.com/star_audb_govexec_com.key'
